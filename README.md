224edd15bd230188720a42a7cb9496335adecafd
Magnus Persson <magnus.e.persson@gmail.com>
2011-09-18 23:08:58 +0200

    Added a simple setup.py script

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-Port of [Robert Penners Signals](https://github.com/robertpenner/as3-signals)
+Python port of [Robert Penners Signals](https://github.com/robertpenner/as3-signals)
 
 Mimics the AS3 API variant but uses lower case with underscore instead of CamelCase.
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name = "pysignals",
+	version = "SNAPSHOT",
+	description = "Simple message dispatcher",
+	author = "Magnus Persson",
+	author_email = "magnus.e.persson@gmail.com",
+	url = "https://github.com/c0dem4gnetic/PySignals",
+    packages = ['signals']
+)
